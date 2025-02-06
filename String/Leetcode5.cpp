d9dc58ee638a84eae54177985c36ddc9ec123412
halwinsfwrks <minhduc.fwrks@gmail.com>
2025-02-06 21:22:44 +0700

    practice leetcode 5

---
+++ String/Leetcode5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        
+        int start = 0, maxLen = 1;
+
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            expand(i, i);     
+            expand(i, i + 1); 
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
