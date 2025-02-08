db3cbe5ebb34a0fcf09c9ff4adb411b01e0218d9
halwinfwrks <minhduc.fwrks@gmail.com>
2025-02-09 00:55:45 +0700

    practice leetcode 38

---
+++ String/Leetcode38.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string countAndSay(int n)
+    {
+        string result = "1";
+        string tmp = "";
+        int slow = 0, fast = 0;
+        for (int i = 1; i < n; i++)
+        {
+            while (fast < result.size())
+            {
+                if (result[fast] == result[slow])
+                    fast++;
+                else
+                {
+                    tmp += to_string(fast - slow) + result[slow];
+                    slow = fast;
+                }
+            }
+            tmp += to_string(fast - slow) + result[slow];
+            result = tmp;
+            tmp = "";
+            slow = fast = 0;
+        }
+        return result;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+
+    Solution s;
+    cout << s.countAndSay(4);
+    return 0;
+}
